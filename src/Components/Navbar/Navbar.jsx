ac5a33bbc2c1c85564f5c4d49dbc6560c0399c07
Eldira Safargalina <eldira-safargalina@mail.ru>
2025-09-03 22:09:27 +0900

    Initial commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import './Navbar.css'
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
 
 export const Navbar = () => {
   return (
-    <div>
-        
+    <div className='navbar'>
+      <div className='top-bar'>
+        <h1>DEAR ELIA</h1>
+        <ul>
+            <li>LOGIN</li>
+            <li>JOIN</li>
+            <li>JOIN</li>
+          </ul>
+      </div>
+      <div className='bottom-bar'>
+      </div>
     </div>
-  )
+  ) 
 }
