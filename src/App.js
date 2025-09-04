aaba96c6a5aefea5ee6a437d161e9a49f8cebf91
Eldira Safargalina <eldira-safargalina@mail.ru>
2025-09-04 22:34:00 +0900

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import './App.css';
 import { Navbar } from './Components/Navbar/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ShopCategory from './Pages/ShopCategory';
+import ShopHomePage from './Pages/ShopHomePage';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+import LoginSignup from './Pages/LoginSignup';
+import JoinUs from './Pages/JoinUs';
+
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<ShopHomePage/>}/>
+        <Route path='/new' element={<ShopCategory category="new"/>}/>
+        <Route path='/best' element={<ShopCategory category="best"/>}/>
+        <Route path='/dress' element={<ShopCategory category="dress"/>}/>
+        <Route path='/outer' element={<ShopCategory category="outer"/>}/>
+        <Route path='/top' element={<ShopCategory category="top"/>}/>
+        <Route path='/acc/bag' element={<ShopCategory category="acc/bag"/>}/>
+        <Route path='/shoes' element={<ShopCategory category="shoes"/>}/>
+
+        <Route path='/product' element={<Product/>}>
+          <Route path=':productId' element={<Product/>}/>
+        </Route>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<LoginSignup/>}/>
+        <Route path='/joinus' element={<JoinUs/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,20 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Navbar.css'
-import logo from '../Assets/logo.png'
+import logo from '../Assets/logo_elia.png'
 import cart_icon from '../Assets/cart_icon.png'
+import { Link } from 'react-router-dom'
 
 export const Navbar = () => {
+
+  //For underlining the menu when you click on it
+  const [menu,setMenu] = useState("shop");
+
   return (
     <div className='navbar'>
-      <div className='top-bar'>
-        <h1>DEAR ELIA</h1>
-        <ul>
-            <li>LOGIN</li>
-            <li>JOIN</li>
-            <li>JOIN</li>
-          </ul>
+      <div className='nav-logo'>
+        <Link to='/'><img src={logo} alt=""/></Link>
       </div>
-      <div className='bottom-bar'>
+      <ul className='nav-menu'>
+        <li onClick={()=>{setMenu("new")}}><Link style={{textDecoration: 'none', color: '#e86b67'}} to='/new'>NEW</Link>{menu=="new"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("best")}}><Link style={{textDecoration: 'none', color: '#e86b67'}} to='/best'>BEST</Link>{menu=="best"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("dress")}}><Link style={{textDecoration: 'none', color: '#e86b67'}} to='/dress'>DRESS</Link>{menu=="dress"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("outer")}}><Link style={{textDecoration: 'none', color: '#e86b67'}} to='/outer'>OUTER</Link>{menu=="outer"?<hr/>:<></>}</li> 
+        <li onClick={()=>{setMenu("top")}}><Link style={{textDecoration: 'none', color: '#e86b67'}} to='/top'>TOP</Link>{menu=="top"?<hr/>:<></>}</li> 
+        <li onClick={()=>{setMenu("acc/bag")}}><Link style={{textDecoration: 'none', color: '#e86b67'}} to='/acc/bag'>ACC/BAG</Link>{menu=="acc/bag"?<hr/>:<></>}</li> 
+        <li onClick= {()=>{setMenu("shoes")}}><Link style={{textDecoration: 'none', color: '#e86b67'}} to='/shoes'>SHOES</Link>{menu=="shoes"?<hr/>:<></>}</li>
+      </ul>
+      <div className='nav-login-cart'>
+        <ul className='nav-login-list'>
+          <Link style={{textDecoration: 'none', color: '#e86b67'}} to='/login'><li>LOGIN</li></Link>
+          <Link style={{textDecoration: 'none', color: '#e86b67'}} to='/joinus'><li>JOIN US</li></Link>
+        </ul>
+        <Link to='/cart'><img src={cart_icon} alt=''/></Link>
+        <div className='nav-cart-count'>0</div>
       </div>
     </div>
   ) 
