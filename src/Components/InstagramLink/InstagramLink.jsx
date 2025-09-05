fb7fe2585abb206d40f8bf94402f47771304216e
Eldira Safargalina <eldira-safargalina@mail.ru>
2025-09-05 21:39:11 +0900

    Initial commit

--- src/Components/InstagramLink/InstagramLink.jsx
+++ src/Components/InstagramLink/InstagramLink.jsx
@@ -6,12 +6,12 @@ import './InstagtramLink.css'
 const InstagramLink = () => {
   return (
     <div className='insta-link'>
-    <h1>@dear_elia.official</h1>
-    <div className='item-insta'>
-        {data_product.map((item) => (
-          <Item 
-          image={item.image}  
-        />
+      <h1>@dear_elia.official</h1>
+      <div className='item-insta'>
+        {data_product.map((item, i) => (
+          <figure className="ig-item" key={i}>
+            <img src={item.image} alt="" loading="lazy" />
+          </figure>
         ))}
       </div>  
     </div>
